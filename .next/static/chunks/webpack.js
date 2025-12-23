121f1ddfd8c6540dd5213eb8043816dbc71620a0
felipe <mdfelipe12@outlook.com>
2025-12-23 00:28:07 -0300

    teste

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "881b82a4e2f74558"; }
+/******/ 		__webpack_require__.h = function() { return "ffab23b4cb6a5ee2"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
