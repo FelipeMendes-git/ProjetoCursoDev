00b0b6492bb6efab224de169ab080ac5a596ce2a
felipe <mdfelipe12@outlook.com>
2025-12-29 17:07:41 -0300

    adição editorconfig e pag em construção

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,35 @@
+const containerStyle = { 
+    backgroundColor: '#0639a0ff',
+    height: '100vh',
+    width: '100vw',
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    justifyContent: 'center',
+    alignItems: 'center',
+    display: 'flex',
+    flexDirection: 'column',
+    fontFamily: 'Arial, sans-serif'
+};
+
+const cardStyle = {
+    background: '#ffffff',
+    padding: '40px',
+    borderRadius: '12px',
+    color: '#333'
+};
+
 function Home(){
-    return <h1>testeeee</h1>
+    return (
+        <div style={containerStyle}>
+
+            <div style={cardStyle}>
+                <h1>Site em construção</h1>
+                <p>volte em alguns meses para ver o site completo.</p>
+            </div>
+
+        </div>
+    )
 }
 
 export default Home
