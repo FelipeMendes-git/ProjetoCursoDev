6126d1415862a81a17a665456c27427259029b6e
felipeMendes <mdfelipe12@outlook.com>
2026-01-17 14:32:10 -0300

    variaveis de ambiente e database.js

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,4 @@
-test("GET to /api/vi/status expect to be 200", async () => {
+test("GET to /api/v1/status expect to be 200", async () => {
     const response = await fetch("http://localhost:3000/api/v1/status")
     expect(response.status).toBe(200);
-})
+});
